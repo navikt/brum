e277afe0256b35257307103563d2a5c6b883003e
Kent Daleng <kent.daleng@nav.no>
2025-06-12 14:33:46 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+
+val ktorVersion = "3.1.3"
+val logbackVersion = "1.5.18"
+val logstashEncoderVersion = "8.1"
+val junitVersion = "5.12.2"
+val mainClassName = "no.nav.MainKt"
+
+plugins {
+    kotlin("jvm") version "2.1.21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+tasks {
+    withType<Jar> {
+        archiveBaseName.set("app")
+
+        manifest {
+            attributes["Main-Class"] = mainClassName
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
+
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("${layout.buildDirectory.get()}/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            showExceptions = true
+        }
+    }
+
+    withType<Wrapper> {
+        gradleVersion = "8.12"
+    }
+}
