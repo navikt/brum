3619a3e19c2d8279f638773e8d3489cb2a18d6c6
Ingvild Utvik <ingvild.utvik@nav.no>
2025-06-25 09:27:34 +0200

    trying with root as remote path

--- src/main/kotlin/no/nav/Main.kt
+++ src/main/kotlin/no/nav/Main.kt
@@ -82,7 +82,7 @@ fun Application.module() {
     }
 
     routing {
-        staticFiles(remotePath = "/files", dir= File("files"))
+        staticFiles(remotePath = "", dir= File("/files"))
 
         authenticate("auth-bearer") {
             get("/testAuth") {
