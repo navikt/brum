141918e6bb4cec4f086dad7c08cc8c223f8e293b
Nahom Berhane <nahom.berhane@nav.no>
2025-06-30 14:36:00 +0200

    removed staticFiles

--- src/main/kotlin/no/nav/Routing.kt
+++ src/main/kotlin/no/nav/Routing.kt
@@ -1,39 +1,53 @@
 package no.nav
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.auth.*
+import io.ktor.server.http.content.staticFiles
 import no.nav.models.AuthenticatedUser
 
 fun Application.configureRouting() {
-    routing {
-        authenticate("auth-bearer") {
-            get("/testAuth") {
-                logger.info("ok")
-                call.respond("ok")
-            }
-        }
-
-        authenticate("auth-bearer") {
-            get("/userInfo") {
-                val user = call.principal<AuthenticatedUser>() ?: error("No authenticated user")
-                call.respond(
-                    mapOf(
-                        "oid" to user.oid,
-                        "username" to user.username,
-                        "groups" to user.groups
-                    )
-                )
-            }
-        }
-
-        get("/testData") {
-            logger.info("data requested")
-            call.respond(getTestData())
-
+    routing {   authenticate("auth-bearer") {
+        get("/testAuth") {
+            logger.info("ok")
+            call.respond("ok")
         }
     }
 
+        staticFiles("/testData", File("files"))
+
+        get("/auth") {
+            logger.info("Request received")
+            call.respond(
+                mapOf(
+                    "active" to true
+                )
+            )
+        }
+            get("/auth") {
+                logger.info("Request received")
+                call.respond(
+                    mapOf(
+                        "active" to true
+                    )
+                )
+            }
+
+
+        authenticate("auth-bearer") {
+                get("/userInfo") {
+                    val user = call.principal<AuthenticatedUser>() ?: error("No authenticated user")
+                    call.respond(
+                        mapOf(
+                            "oid" to user.oid,
+                            "username" to user.username,
+                            "groups" to user.groups
+                        )
+                    )
+                }
+        }
+    }
 }
 
