753f4e4c2a8dfb26741c2f7106f9038422b54fe0
Aksel Lessum <aksel.henrik.lessum@nav.no>
2025-07-01 10:28:55 +0200

    added routing back

--- src/main/kotlin/no/nav/Routing.kt
+++ src/main/kotlin/no/nav/Routing.kt
@@ -1,7 +1,9 @@
 package no.nav
 
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
+import io.ktor.server.application.call
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.auth.*
@@ -14,6 +16,18 @@ fun Application.configureRouting() {
             get("/testAuth") {
                 logger.info("ok")
                 call.respond("ok")
+            }
+        }
+
+        get("/gjennomforing") {
+            logger.info("ok")
+            //call.respond("ok")
+            try {
+                val result = getGjennomforinger("brum-dev-b72f")
+                call.respondText(result, ContentType.Text.Plain)
+            } catch (e: Exception) {
+                logger.error("Feil ved henting av gjennomforing", e)
+                call.respond(HttpStatusCode.InternalServerError, "Feil ved henting av gjennomforing: ${e.message}")
             }
         }
 
