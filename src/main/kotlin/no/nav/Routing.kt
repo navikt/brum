2c8f9844273dd8cf78d4c040ed26c7661d48b068
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-02 10:35:05 +0200

    minor cleanup

--- src/main/kotlin/no/nav/Routing.kt
+++ src/main/kotlin/no/nav/Routing.kt
@@ -5,7 +5,6 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.auth.*
-import io.ktor.server.http.content.staticFiles
 import no.nav.models.AuthenticatedUser
 
 fun Application.configureRouting() {
