78f3860d6c7cd0f74b4a19ac7198195fba1d28aa
Nahom Berhane <nahom.berhane@nav.no>
2025-07-07 11:38:50 +0200

    simplfied user info response.

--- src/main/kotlin/no/nav/models/AuthenticatedUser.kt
+++ src/main/kotlin/no/nav/models/AuthenticatedUser.kt
@@ -6,14 +6,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class AuthenticatedUser(
-    val oid: String,
     val username: String,
-    val groups: List<String>,
-    val azp: String? = null,
-    val azp_name: String? = null,
-    val NAVident: String? = null,
-    val roles: List<String> = emptyList(),
-    val scp: List<String> = emptyList(),
-    val idtyp: String? = null
-
+    val NAVident: String,
+    val name: String,
 )
