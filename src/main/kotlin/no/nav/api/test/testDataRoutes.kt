ca9b3ea4eae112bdeb238386df453bb9e6ee9096
Ingvild Utvik <ingvild.utvik@nav.no>
2025-07-11 10:09:45 +0200

    serving testdataset mini as json

--- src/main/kotlin/no/nav/api/test/testDataRoutes.kt
+++ src/main/kotlin/no/nav/api/test/testDataRoutes.kt
@@ -2,6 +2,7 @@ package no.nav.api.test
 
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.Application
 import io.ktor.server.request.uri
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
@@ -13,7 +14,7 @@ fun Route.testDataRoutes() {
         val dataset = call.request.queryParameters["dataset"]
         when (dataset) {
             "Real" -> call.respondText(getRealTestData(), contentType = ContentType.Text.CSV)
-            "Mini.csv" -> call.respondText(getMiniCsv(), contentType = ContentType.Text.CSV)
+            "Mini" -> call.respond(getMiniCsv())
             else -> call.respondText(
                 "Ugyldig datasettnummer. Datasettet : $dataset. finnes ikke. Fullstendig forespørsel: ${call.request.uri}",
                 status = HttpStatusCode.BadRequest
--- src/main/kotlin/no/nav/data/GetTestData.kt
+++ src/main/kotlin/no/nav/data/GetTestData.kt
@@ -1,11 +1,16 @@
 package no.nav.data
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
+private val objectMapper = jacksonObjectMapper()
+
 fun getRealTestData(): String {
     return object {}::class.java.getResource("/testdata/real-test-data.csv").readText()
 }
 
-fun getMiniCsv(): String {
-    return object {}::class.java.getResource("/testdata/mini.csv").readText();
+fun getMiniCsv(): Map<String, Array<Any>> {
+    return objectMapper.readValue(object {}::class.java.getResource("/testdata/mini.json").readText());
 }
 
 
