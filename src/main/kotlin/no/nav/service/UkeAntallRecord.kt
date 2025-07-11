99baa79a207d7c85e938b46d4f13b4dc1da02872
Aksel Lessum <aksel.henrik.lessum@nav.no>
2025-07-11 10:37:04 +0200

    removed limit 10 on sql select

--- src/main/kotlin/no/nav/service/UkeAntallRecord.kt
+++ src/main/kotlin/no/nav/service/UkeAntallRecord.kt
@@ -20,7 +20,6 @@ class UkeAntallService() {
         `år`, `uke`, `tiltaksnavn`, `innsatsgruppe`, `avdeling`, `antall`
       FROM `brum-dev-b72f.tiltak_gold.uke_antall_gold_mock`
       WHERE `år` = $år AND `uke` = $uke
-      LIMIT 10
     """.trimIndent()
 
         val rows = queryRunner.runQuery(sql, prosjektId)
